84cd95731fb980d7430cef6df01b93e1ca76162a
Antonio Stassi <stassiant@outlook.it>
2024-10-16 18:26:04 +0200

    tilde on title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Comics - @yield('website-title')</title>
+        <title>Comics ~ @yield('website-title')</title>
         @vite('resources/js/app.js')
         @yield('head-imports')
 
