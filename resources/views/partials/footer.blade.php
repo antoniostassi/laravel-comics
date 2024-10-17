f50cdeb7d397db4021ce149938041e6f63981291
Antonio Stassi <stassiant@outlook.it>
2024-10-17 14:13:56 +0200

    footer completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -49,12 +49,31 @@
                         <li>Test</li>
                     </ul>
                 </div>
+                <p class="pt-3" style="color:gray;">All site content TM Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui porro alias amet aut. Quasi, voluptatum?</p>
             </div>
             <div class="col-6 big-logo-dc">
                 <img src="/images/dc-logo-bg.png" alt="DC Logo BG">
             </div>
         </div>
-        
+    
+    </div>
+</div>
+
+<div class="footer-end py-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-6">
+                <div class="btn border-primary text-white"> SIGN UP NOW!</div>
+            </div>
+            <div class="col-6 d-flex justify-content-end gap-3">
+                <h4 class="text-primary position-relative" style="top:5px; font-size:1.2rem;"><strong>FOLLOW US</strong></h4>
+                <img src="/images/footer-facebook.png" alt="Facebook">
+                <img src="/images/footer-periscope.png" alt="periscope">
+                <img src="/images/footer-pinterest.png" alt="pinterest">
+                <img src="/images/footer-twitter.png" alt="twitter">
+                <img src="/images/footer-youtube.png" alt="youtube">
+            </div>
+        </div>
     </div>
 </div>
 
@@ -80,6 +99,19 @@
                 width:100%;
                 object-fit:cover;
             }
+        }
+    }
+
+    .footer-end {
+        width:100%;
+        background-color:#303030;
+
+        .btn-comics {
+            padding:10px;
+        }
+
+        .col-6.d-flex img {
+            margin:0 10px;
         }
     }
 </style>
