0d48e16386140c76edb39dc424ed2c748316b794
Antonio Stassi <stassiant@outlook.it>
2024-10-17 13:57:59 +0200

    services-list + big logo cut on y coords

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,52 +1,60 @@
 <div class="w-100">
-    <div class="container text-white py-5">
-        <div class="services-list d-flex gap-5">
-            <div class="list-1">
-                <h5>DC COMICS</h5>
-                <ul>
-                    <li>Characters</li>
-                    <li>Characters</li>
-                    <li>Characters</li>
-                    <li>Characters</li>
-                    <li>Characters</li>
-                    <li>Characters</li>
-                </ul>
+    <div class="container text-white">
+        <div class="row">
+            <div class="col-6 pt-5">
+                <div class="services-list d-flex gap-5">
+                    <div class="list-1">
+                        <h5>DC COMICS</h5>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Characters</li>
+                            <li>Characters</li>
+                            <li>Characters</li>
+                            <li>Characters</li>
+                            <li>Characters</li>
+                        </ul>
+                    </div>
+                    <div class="list-2">
+                        <h5>DC</h5>
+                        <ul>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                            <li>Terms of Use</li>
+                        </ul>
+                    </div>
+                    <div class="list-3">
+                        <h5>SITES</h5>
+                        <ul>
+                            <li>DC</li>
+                            <li>DC</li>
+                            <li>DC</li>
+                            <li>DC</li>
+                            <li>DC</li>
+                            <li>DC</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="list-4">
+                    <h4>List 4</h4>
+                    <ul>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                    </ul>
+                </div>
             </div>
-            <div class="list-2">
-                <h5>DC</h5>
-                <ul>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                    <li>Terms of Use</li>
-                </ul>
-            </div>
-            <div class="list-3">
-                <h5>SITES</h5>
-                <ul>
-                    <li>DC</li>
-                    <li>DC</li>
-                    <li>DC</li>
-                    <li>DC</li>
-                    <li>DC</li>
-                    <li>DC</li>
-                </ul>
+            <div class="col-6 big-logo-dc">
+                <img src="/images/dc-logo-bg.png" alt="DC Logo BG">
             </div>
         </div>
-        <div class="list-4">
-            <h4>List 4</h4>
-            <ul>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-                <li>Test</li>
-            </ul>
-        </div>
+        
     </div>
 </div>
 
@@ -55,6 +63,7 @@
 <style lang="scss"> 
     footer > .w-100 {
         background-image:url("/images/footer-bg.jpg");
+        background-size:cover;
 
         h5 {
             font-weight:bold;
@@ -62,6 +71,15 @@
 
         ul li {
             color:gray;
+        }
+        
+        .big-logo-dc {
+            height:450px;
+            &>img {
+                height:100%;
+                width:100%;
+                object-fit:cover;
+            }
         }
     }
 </style>
